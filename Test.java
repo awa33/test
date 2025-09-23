42e7db5ec944551b74eb64595010e5db596993da
awa33 <66191215+awa33@users.noreply.github.com>
2025-09-23 19:23:55 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,15 @@
+public class Test {
+
+  static void add(){
+
+    int a=10;
+    int b=20;
+    int result = a+b;
+    System.out.println("The sum is " +result);
+    
+  }
+  public static void main(String[] args)
+  {
+    add();
+  }
+}
